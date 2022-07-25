b00b0d03b236aa0b1c34c5608b62dec866ab51a0
yutabee <bsk87m@gmail.com>
2022-07-25 21:35:32 +0900

    frontend

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
 
 export const siteTitle = "Next.js blog";
 const name = 'yuuutabee';
@@ -7,19 +9,20 @@ const name = 'yuuutabee';
 function Layout({children}) {
 
     return (
-        <div>
+        <div className={styles.container}>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <header>
-                {/* <img src="/images/profile.png" /> */}
+            <header className={styles.header}>
+                {/* <img src="/images/profile.jpg" className={utilStyles.borderCircle}/> */}
                  <Image
                     src="/images/profile.jpg"
                     alt="profileimg"
                     width={50}
                     height={50}
+                    className={utilStyles.borderCircle}
                 />
-                <h1>{name}</h1>
+                <h1 className={utilStyles.heading2Xl}>{name}</h1>
             </header>
             <main>{children}</main>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,91 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import styles from '../styles/Home.module.css'
+import { LocalAirport, Memory } from '@mui/icons-material';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import styles from '../styles/Home.module.css';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
-    <Layout>content</Layout>
+    <Layout>
+      <section className={utilStyles.headingMd}>
+        <p>私は駆け出しエンジニアです。前職は薬剤師をしていました。</p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2><Memory/>旅好きエンジニアのブログ<LocalAirport/></h2>
+      <div className={styles.grid}>
+        <article>
+          <Link href='/'>
+            <Image
+              src='/images/bailey-zindel-NRQV-hBF10M-unsplash.jpg'
+              alt=''
+              width={300}
+              height={200}
+              className={styles.thumbnailImage}  
+            />
+          </Link>
+          <br/>
+          <Link href='/'>
+            <a className={utilStyles.boldText }>Yosemite国立公園に行ってみた話</a>
+          </Link>
+          <br/>
+          <small className={utilStyles.lightText}>February 23, 2020</small>
+        </article>
+        <article>
+          <Link href='/'>
+            <Image
+              src='/images/bailey-zindel-NRQV-hBF10M-unsplash.jpg'
+              alt=''
+              width={300}
+              height={200}
+              className={styles.thumbnailImage}  
+            />
+          </Link>
+          <br/>
+          <Link href='/'>
+            <a className={utilStyles.boldText }>Yosemite国立公園に行ってみた話</a>
+          </Link>
+          <br/>
+          <small className={utilStyles.lightText}>February 23, 2020</small>
+        </article>
+        <article>
+          <Link href='/'>
+            <Image
+              src='/images/bailey-zindel-NRQV-hBF10M-unsplash.jpg'
+              alt=''
+              width={300}
+              height={200}
+              className={styles.thumbnailImage}  
+            />
+          </Link>
+          <br/>
+          <Link href='/'>
+            <a className={utilStyles.boldText }>Yosemite国立公園に行ってみた話</a>
+          </Link>
+          <br/>
+          <small className={utilStyles.lightText}>February 23, 2020</small>
+        </article>
+        <article>
+          <Link href='/'>
+            <Image
+              src='/images/bailey-zindel-NRQV-hBF10M-unsplash.jpg'
+              alt=''
+              width={300}
+              height={200}
+              className={styles.thumbnailImage}  
+            />
+          </Link>
+          <br/>
+          <Link href='/'> 
+            <a className={utilStyles.boldText }>Yosemite国立公園に行ってみた話</a>
+          </Link>
+          <br/>
+          <small className={utilStyles.lightText}>February 23, 2020</small>
+        </article>
+        </div>
+      </section>
+    </Layout>
   )
 }

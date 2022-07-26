2dde86e624fa4464e46479a6aecc1affe7c134d7
yutabee <bsk87m@gmail.com>
2022-07-26 12:36:12 +0900

    blog article

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,31 @@ import Link from 'next/link';
 import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css';
 import utilStyles from '../styles/utils.module.css';
+import { getPostsData } from '../lib/post';
 
-export default function Home() {
+//SSGの場合(getStaticPropsはNexjsのfunction>一度だけ外部から静的ファイルを読み込む)
+export async function getStaticProps() {
+  const allPostsData = getPostsData();  //id,title,data,thumbnail
+  // console.log(allPostsData);
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+// //SSRの場合
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       //コンポーネントに渡すためのprops
+
+//     },
+//   };
+// }
+
+export default function Home({ allPostsData }) {
   return (
     <Layout>
       <section className={utilStyles.headingMd}>
@@ -15,75 +38,26 @@ export default function Home() {
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2><Memory/>旅好きエンジニアのブログ<LocalAirport/></h2>
-      <div className={styles.grid}>
-        <article>
-          <Link href='/'>
-            <Image
-              src='/images/bailey-zindel-NRQV-hBF10M-unsplash.jpg'
-              alt=''
-              width={300}
-              height={200}
-              className={styles.thumbnailImage}  
-            />
-          </Link>
-          <br/>
-          <Link href='/'>
-            <a className={utilStyles.boldText }>Yosemite国立公園に行ってみた話</a>
-          </Link>
-          <br/>
-          <small className={utilStyles.lightText}>February 23, 2020</small>
+        <div className={styles.grid}>
+          {allPostsData.map(({id,title,date,thumbnail}) => (
+            <article key={id}>
+              <Link href={`/posts/${id}`}>
+                <Image
+                  src={`${thumbnail}`}
+                  alt=''
+                  width={300}
+                  height={200}
+                  className={styles.thumbnailImage}  
+                />
+              </Link>
+              <br/>
+              <Link href={`/posts/${id}`}>
+                <a className={utilStyles.boldText}>{title}</a>
+              </Link>
+              <br/>
+              <small className={utilStyles.lightText}>{date}</small>
         </article>
-        <article>
-          <Link href='/'>
-            <Image
-              src='/images/bailey-zindel-NRQV-hBF10M-unsplash.jpg'
-              alt=''
-              width={300}
-              height={200}
-              className={styles.thumbnailImage}  
-            />
-          </Link>
-          <br/>
-          <Link href='/'>
-            <a className={utilStyles.boldText }>Yosemite国立公園に行ってみた話</a>
-          </Link>
-          <br/>
-          <small className={utilStyles.lightText}>February 23, 2020</small>
-        </article>
-        <article>
-          <Link href='/'>
-            <Image
-              src='/images/bailey-zindel-NRQV-hBF10M-unsplash.jpg'
-              alt=''
-              width={300}
-              height={200}
-              className={styles.thumbnailImage}  
-            />
-          </Link>
-          <br/>
-          <Link href='/'>
-            <a className={utilStyles.boldText }>Yosemite国立公園に行ってみた話</a>
-          </Link>
-          <br/>
-          <small className={utilStyles.lightText}>February 23, 2020</small>
-        </article>
-        <article>
-          <Link href='/'>
-            <Image
-              src='/images/bailey-zindel-NRQV-hBF10M-unsplash.jpg'
-              alt=''
-              width={300}
-              height={200}
-              className={styles.thumbnailImage}  
-            />
-          </Link>
-          <br/>
-          <Link href='/'> 
-            <a className={utilStyles.boldText }>Yosemite国立公園に行ってみた話</a>
-          </Link>
-          <br/>
-          <small className={utilStyles.lightText}>February 23, 2020</small>
-        </article>
+          ))}
         </div>
       </section>
     </Layout>
